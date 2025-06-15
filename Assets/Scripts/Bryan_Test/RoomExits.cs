3cc73e0eb43d5a20ae164fc4d457de6a756cbdc1
Bryan Berry <bryanberry590@gmail.com>
2025-06-15 10:27:35 -0700

    WE HAVE INTERPOLATION

--- Assets/Scripts/Bryan_Test/RoomExits.cs
+++ Assets/Scripts/Bryan_Test/RoomExits.cs
@@ -5,8 +5,13 @@ public class RoomExits : MonoBehaviour
 {
     [SerializeField] private Vector2 cameraPanDistance;
     [SerializeField] private Camera mainCamera;
+    private Vector3 oldCameraPosition;
     [SerializeField] private Vector2 newPlayerPosition;
     [SerializeField] private Vector2 oldPlayerPosition;
+    [SerializeField] private float cameraLerpDuration;
+    private Vector3 newCameraPosition;
+    private float timeElapsed = 0f;
+    private bool lerpInProgress = false;
 
     void Start()
     {
@@ -17,6 +22,27 @@ public class RoomExits : MonoBehaviour
     {
         mainCamera = Camera.main;
     }
+
+    private void Update()
+    {
+        if (lerpInProgress)
+        {
+            Debug.Log("Lerp is in progress.");
+            timeElapsed += Time.deltaTime;
+            float t = timeElapsed / cameraLerpDuration;
+            mainCamera.gameObject.transform.position = Vector3.Lerp(oldCameraPosition, newCameraPosition, t);
+            Debug.Log("new camera position is : " + mainCamera.gameObject.transform.position);
+        }
+
+        if (timeElapsed >= cameraLerpDuration)
+        {
+            timeElapsed = 0f;
+            lerpInProgress = false;
+        }
+    }
+    
+    
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(!collision.CompareTag("Player"))return;
@@ -27,12 +53,21 @@ public class RoomExits : MonoBehaviour
         collision.gameObject.transform.position = new Vector2(oldPlayerPosition.x + newPlayerPosition.x, oldPlayerPosition.y + newPlayerPosition.y);
         
         //interpolate the camera to the next point
+        LerpCamera();
+
         
-        Debug.Log("moving camera");
-        Vector3 newCameraPosition = mainCamera.transform.position;
-        newCameraPosition = new Vector3(cameraPanDistance.x + newCameraPosition.x, cameraPanDistance.y + newCameraPosition.y, newCameraPosition.z);
-        mainCamera.transform.position = newCameraPosition;
         
         //
+    }
+
+    private void LerpCamera()
+    {
+        Debug.Log("moving camera");
+        oldCameraPosition = mainCamera.transform.position;
+        newCameraPosition = new Vector3(cameraPanDistance.x + oldCameraPosition.x, cameraPanDistance.y + oldCameraPosition.y, oldCameraPosition.z);
+        lerpInProgress = true;
+        
+        //interpolate from oldCameraPos to new CameraPos
+        //mainCamera.transform.position = newCameraPosition;
     }
 }
