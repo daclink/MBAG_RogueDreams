27003ea570a1c2268965feab01bc989df84f04d0
Bryan Berry <bryanberry590@gmail.com>
2025-07-05 13:55:09 -0700

    Added a player health manager
    Created a player health manager that will listen to all events for added and removing health from the player and will also kill the player through an event or directly. I am waiting on PR from kyle to tie this to the enemy class he is making and I am waiting on the PR to be accepted from Jackie to tie this to the healing item object.

---
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    [SerializeField] private int startHealth = 100;
+    private int currHealth;
+    
+    void Start()
+    {
+        SetHealth(startHealth);
+        //listen to 2 events
+        //  - one event for adding health from the healing items
+        //  - one event for taking damage from the enemy class
+    }
+    
+
+    void SetHealthBar(int health)
+    {
+        //set the healthBarVisual here
+    }
+    
+    /**
+     * This function sets the player health directly,
+     * does not add or subtract health
+     * int health is the int value to set the health to
+     */
+    void SetHealth(int health)
+    {
+        currHealth = health;
+        SetHealthBar(currHealth);
+    }
+
+    /**
+     * This function takes in a health value and adds it to the current health
+     * and checks that health does not go over the max
+     * int health is the passed in int value to add to the current health
+     */
+    void AddHealth(int health)
+    {
+        currHealth += health;
+        if (currHealth > startHealth)
+        {
+            currHealth = startHealth;
+        }
+        SetHealthBar(currHealth);
+    }
+
+    /**
+     * This function takes in a health value and subtracts it from the players
+     * current health and makes sure health does not go below 0
+     * int health is the passed in int value to subtract from the current health
+     */
+    void RemoveHealth(int health)
+    {
+        currHealth -= health;
+        if (currHealth <= 0)
+        {
+            currHealth = 0;
+            SetHealthBar(currHealth);
+            // kill player here either directly or through an event to another script
+            return;
+        }
+        SetHealthBar(currHealth);
+        
+    }
+
+
+}
